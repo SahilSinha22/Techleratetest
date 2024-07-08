430d8b5c462832f0bbba7ebc85772024c7221216
sahil sinha <sahilsinha2204@gmail.com>
2024-07-08 09:14:57 +0530

    comp

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -8,19 +8,19 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer class="bg-black text-card-foreground py-8 p-12">
-    <div class="container  ">
-      <div class="flex border-t  mx-10 pt-20  border-gray-400 flex-wrap pb-12 justify-between">
-        <div class="w-full md:w-2/4 mb-6  md:mb-0 ">
-          <div class="flex items-center mb-4">
-            <Image src="" alt="Techlerate Logo" class="mr-2" width={40} height={2}/>
-            <span class="font-bold text-white text-lg">TECHLERATE</span>
+    <footer className="bg-black text-card-foreground py-8 p-12">
+    <div className="container  ">
+      <div className="flex border-t  mx-10 pt-20  border-gray-400 flex-wrap pb-12 justify-between">
+        <div className="w-full md:w-2/4 mb-6  md:mb-0 ">
+          <div className="flex items-center mb-4">
+            <Image src="" alt="Techlerate Logo" className="mr-2" width={40} height={2}/>
+            <span className="font-bold text-white text-lg">TECHLERATE</span>
           </div>
-          <p class="text-blue-800  ">Connect with us:</p>
-          <p class="text-slate-400 ">Call: +91 88 820 76 755</p>
-          <p class="text-slate-400 ">Email: amit@spacetotech.com</p>
-          <p class="text-slate-400">Office: C-171 Sector-63 Noida, Uttar Pradesh 201301</p>
-          <div class="flex mt-6 space-x-4 ">
+          <p className="text-blue-800  ">Connect with us:</p>
+          <p className="text-slate-400 ">Call: +91 88 820 76 755</p>
+          <p className="text-slate-400 ">Email: amit@spacetotech.com</p>
+          <p className="text-slate-400">Office: C-171 Sector-63 Noida, Uttar Pradesh 201301</p>
+          <div className="flex mt-6 space-x-4 ">
             <a href="#" aria-label="Instagram">
               <Image src={Insta} alt="Instagram Icon" className='rounded-full' width={40} height={2}/>
             </a>
@@ -36,42 +36,42 @@ const Footer = () => {
           </div>
         </div>
   
-        <div class="w-full md:w-2/4 flex flex-wrap">
-          <div class="w-full sm:w-1/3 mb-6 sm:mb-0">
-            <h3 class=" text-white mb-4">Solution</h3>
-            <ul class="space-y-2">
-              <li><a href="#" class="text-white hover:text-primary">Brand & Design</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Video Production</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Social Media</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Digital Campaign</a></li>
-              <li><a href="#" class="text-white hover:text-primary">UI/UX</a></li>
-              <li><a href="#" class="text-white hover:text-primary">SEO</a></li>
+        <div className="w-full md:w-2/4 flex flex-wrap">
+          <div className="w-full sm:w-1/3 mb-6 sm:mb-0">
+            <h3 className=" text-white mb-4">Solution</h3>
+            <ul className="space-y-2">
+              <li><a href="#" className="text-white hover:text-primary">Brand & Design</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Video Production</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Social Media</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Digital Campaign</a></li>
+              <li><a href="#" className="text-white hover:text-primary">UI/UX</a></li>
+              <li><a href="#" className="text-white hover:text-primary">SEO</a></li>
             </ul>
           </div>
-          <div class="w-full sm:w-1/3 mb-6 sm:mb-0">
-            <h3 class=" text-white mb-4">Company</h3>
-            <ul class="space-y-2">
-              <li><a href="#" class="text-white hover:text-primary">About</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Contact</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Testimonial</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Partners</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Portfolio</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Careers</a></li>
+          <div className="w-full sm:w-1/3 mb-6 sm:mb-0">
+            <h3 className=" text-white mb-4">Company</h3>
+            <ul className="space-y-2">
+              <li><a href="#" className="text-white hover:text-primary">About</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Contact</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Testimonial</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Partners</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Portfolio</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Careers</a></li>
             </ul>
           </div>
-          <div class="w-full sm:w-1/3">
-            <h3 class=" text-white mb-4">Support</h3>
-            <ul class="space-y-2">
-              <li><a href="#" class="text-white hover:text-primary">Help center</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Terms of service</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Legal</a></li>
-              <li><a href="#" class="text-white hover:text-primary">Privacy policy</a></li>
+          <div className="w-full sm:w-1/3">
+            <h3 className=" text-white mb-4">Support</h3>
+            <ul className="space-y-2">
+              <li><a href="#" className="text-white hover:text-primary">Help center</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Terms of service</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Legal</a></li>
+              <li><a href="#" className="text-white hover:text-primary">Privacy policy</a></li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="mt-8 border-t border-gray-400 pt-8 mx-8 pb-10 text-center">
-        <p class="text-slate-400 text-sm">&copy;2024 TECHLERATE. All rights reserved</p>
+      <div className="mt-8 border-t border-gray-400 pt-8 mx-8 pb-10 text-center">
+        <p className="text-slate-400 text-sm">&copy;2024 TECHLERATE. All rights reserved</p>
       </div>
     </div>
   </footer>
--- components/Started.js
+++ components/Started.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Started = () => {
   return (
    
-    <div class="flex items-center  justify-center max-h-screen py-20  p-20 bg-black ">
-    <div class="bg-violet-800  text-primary-foreground  py-10 shadow-lg w-full text-center">
-      <p class="text-lg text-white  mb-5">MAKING THE RIGHT CHOICES</p>
-      <h2 class="text-6xl font-bold mb-4">LET’S <span class="text-white">START YOUR</span></h2>
-      <h2 class="text-6xl font-bold mb-4"> DIGITAL <span class="text-white">BUSINESS</span></h2>
+    <div className="flex items-center  justify-center max-h-screen py-20  p-20 bg-black ">
+    <div className="bg-violet-800  text-primary-foreground  py-10 shadow-lg w-full text-center">
+      <p className="text-lg text-white  mb-5">MAKING THE RIGHT CHOICES</p>
+      <h2 className="text-6xl font-bold mb-4">LET’S <span className="text-white">START YOUR</span></h2>
+      <h2 className="text-6xl font-bold mb-4"> DIGITAL <span className="text-white">BUSINESS</span></h2>
 
-      <div class="md:flex justify-center mt-10 md:mx-40">
-        <input type="email" placeholder="Enter your email address" class="p-2 border  border-input border-white bg-violet-800 w-auto md:w-3/3" />
-        <button class="bg-white   p-2 ">Get Started</button>
+      <div className="md:flex justify-center mt-10 md:mx-40">
+        <input type="email" placeholder="Enter your email address" className="p-2 border  border-input border-white bg-violet-800 w-auto md:w-3/3" />
+        <button className="bg-white   p-2 ">Get Started</button>
       </div>
     </div>
   </div>
