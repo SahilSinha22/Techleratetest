09f5f3f1f4e42368b3272d7f3e4f50a4603645d6
sahil sinha <sahilsinha2204@gmail.com>
2024-08-12 19:24:19 +0530

    ..

--- components/Comingsoon.jsx
+++ components/Comingsoon.jsx
@@ -6,7 +6,7 @@ const Comingsoon = () => {
     <div>
        <section class="containerx text-white">
       <Image src="/Comingsoon.jpg" alt="" class="image" width={1000} height={1000}/>
-      <header>Comming Soon Page</header>
+      <header>Coming Soon Page</header>
       <p>
         Our website is under construction. We&apos;re working hard to improve our
         website and we &apos;ll ready to launch after.
