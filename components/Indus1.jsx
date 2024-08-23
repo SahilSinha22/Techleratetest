f4f3468469e7f158a3f270ef307c7f44c0aa0530
sahil sinha <sahilsinha2204@gmail.com>
2024-08-23 11:18:53 +0530

    changex

--- components/Indus1.jsx
+++ components/Indus1.jsx
@@ -29,22 +29,22 @@ const Indus = () => {
         panel.removeEventListener("mouseover", handleMouseEnter);
       });
     };
-  }, []); // Empty dependency array means this runs once after initial render
+  }, []);  // Empty dependency array means this runs once after initial render
 
   return (
-    <div className=" md:mx-20 2xl:max-w-7xl lg:mx-24 2xl:mx-52 bg-background open_sans_display  py-12 pr-10 ">
-      <div className="text-left ml-10 md:mx-10">
+    <div className=" max-w-7xl mx-10  lg:px-8 py-12">
+      <div className="text-left ml-10 md:mx-20">
         <h2 className="text-3xl font-bold playfair md:text-6xl text-zinc-900">
           Industries we serve
         </h2>
-        <p className="mt-2 text-lg xl:mr-20 md:text-xl text-zinc-600 ">
+        <p className="mt-2 text-lg md:text-2xl text-zinc-600 ">
           Our top mobile app development company proudly serves a diverse range
           of industries. Through innovative technology solutions, we empower
           businesses in numerous sectors to engage customers, optimize
           operations, and unlock new opportunities for growth.
         </p>
         <div className="galerrys ">
-          <div className="containers mr-10 2xl:mr-28">
+          <div className="containers mr-10">
             <div className="panel active Indus">
               <div className="img-boxs">
                 <h3 className="head">Healthcare</h3>
